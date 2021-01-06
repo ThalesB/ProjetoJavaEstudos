4b8cdb1df5838a23df42fb6455a5a6768abc2027
Thales Batista <thalesbatista18@gmail.com>
2021-01-06 16:57:09 -0300

    Utilizando repository e criando consulta API com filtros.

--- ProjetosJavaEstudos/forum/src/main/java/br/com/alura/forum/controller/TopicosController.java
+++ ProjetosJavaEstudos/forum/src/main/java/br/com/alura/forum/controller/TopicosController.java
@@ -3,6 +3,9 @@ package br.com.alura.forum.controller;
 import java.util.Arrays;
 import java.util.List;
 
+import br.com.alura.forum.repository.TopicoRepository;
+import net.bytebuddy.implementation.bind.MethodDelegationBinder;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,12 +15,20 @@ import br.com.alura.forum.modelo.Topico;
 
 @RestController
 public class TopicosController {
-	
+	@Autowired
+	private TopicoRepository topicoRepository;
+
 	@RequestMapping("/topicos")
-	public List<TopicoDto> lista() {
-		Topico topico = new Topico("Dúvida", "Dúvida com Spring", new Curso("Spring", "Programação"));
-		
-		return TopicoDto.converter(Arrays.asList(topico, topico, topico));
+	public List<TopicoDto> lista(String nomeCurso) {
+        if(nomeCurso == null){
+			List<Topico> topicos = topicoRepository.findAll();
+
+			return TopicoDto.converter(topicos);
+		}else {
+			List<Topico> topicos = topicoRepository.findByCurso_Nome(nomeCurso);
+
+			return TopicoDto.converter(topicos);
+		}
 	}
 
 }
